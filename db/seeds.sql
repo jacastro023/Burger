dfb19da5979faaf82590dfbf0131faed5321a589
Jose Castro <jacastro023@gmail.com>
2020-01-29 11:10:31 -0600

    added all the functionality

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Double Bacon Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Single Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Wendys Burger', true);
+INSERT INTO burgers (name) VALUES ('Mushroom Burger');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,61 @@
+$(function () {
+  $(".eat").on("click", function (event) {
+
+    var id = $(this).data("id");
+    console.log("hello "+id);
+
+    var newEat = $(this).data("neweat");
+    console.log("hello"+$(this).data("neweat"));
+
+
+    var newEatState = {
+      eaten: newEat
+    };
+    console.log(newEatState);
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatState
+    }).then(
+      function () {
+        console.log("changed eat to", newEat);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#ca").val().trim(),
+      eaten: 0
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+  $(".delete").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(function () {
+      console.log("deleted")
+      location.reload();
+    });
+  });
+});
