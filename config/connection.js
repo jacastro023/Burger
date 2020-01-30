33893c990bea7195ff64714515f481aaebf7e14e
Jose Castro <jacastro023@gmail.com>
2020-01-29 22:11:57 -0600

    changed connection again

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection;
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db;"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burger_db"
+// });
 
-// Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -25,7 +25,7 @@ if (process.env.JAWSDB_URL) {
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'burgers_db'
+    database: 'burger_db'
   })
 }
 connection.connect();
